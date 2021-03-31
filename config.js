7ab96b5e10c698fd46eb4f6fffaa2201690238ed
xxzyn <49114258+xxzyn@users.noreply.github.com>
2021-03-31 11:58:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
     token: "TOKEN",
     botname: "Insert Name Here",
 
-    // DO NOT TOUCH | API AUTHORIZATION
-    authorization: "NjgzNjcyNDg0MTE3MjE3MzA4.MTYxNjg1NDUwOTk4NQ==.4e6bf67925535e6389865c184ae9e420"
+    // API AUTHORIZATION
+    authorization: "API KEY"
 }
